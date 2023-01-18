3f4c66ffc295071f9bb5bb18d81063ba11da3186
VIxay SidtialdE <vsidtiladstudent@shacademy.org>
2023-01-18 12:34:12 -0800

    working on image

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,23 @@ class MyHomePage extends StatelessWidget {
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hello'),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemBuilder:  (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8),
+            child: Container(
+              height: 50,
+              width: 50,
+              color: Colors.blueGrey(700),
+              child: Image(image: Image.network('https://m.media-amazon.com/images/I/61MKG7Cg-5L.jpg')),
+            ),
+          )
+        },
+      ),
+    
     );
   }
 }
