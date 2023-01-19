6b279af322228517efc124bd2c1686ac80167b2f
VIxay SidtialdE <vsidtiladstudent@shacademy.org>
2023-01-19 14:24:32 -0800

    working on app state

--- lib/main.dart
+++ lib/main.dart
@@ -9,43 +9,53 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
- const MyApp({super.key});
+  const MyApp({super.key});
 
- @override 
- Widget build(BuildContext context) {
-  return ChangeNotifierProvider(
-    create: (context) => MyAppstate(),
-    child: MaterialApp(
-      title: '1-17 App',
-      theme: ThemeData(),
-      home: MyHomePage(),
-    )
-  );
- }
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (context) => MyAppstate(),
+        child: MaterialApp(
+          title: '1-17 App',
+          theme: ThemeData(),
+          home: MyHomePage(),
+        ));
+  }
 }
-class MyAppstate extends ChangeNotifier {}
+
+class MyAppstate extends ChangeNotifier {
+  var opacityList = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0];
+}
 
 class MyHomePage extends StatelessWidget {
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
         ),
-        itemBuilder:  (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8),
-            child: Container(
-              height: 50,
-              width: 50,
-              color: Colors.blueGrey(700),
-              child: Image(image: Image.network('https://m.media-amazon.com/images/I/61MKG7Cg-5L.jpg')),
+        itemBuilder: (context, index) {
+          return Opacity(
+            opacity: 0.5,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: SizedBox(
+                height: 50,
+                width: 50,
+                child: Material(
+                  color: Colors.blueGrey,
+                  child: InkWell(
+                    child: Image.network(
+                        'https://external-preview.redd.it/CGvAddyJJ__1SbfGCrnHrNKmuQhU8EjruyANV_4wNJA.jpg?auto=webp&s=d721c80ec2f8313992f9bd448016e5a57d623c6ar'),
+                    onTap: () {},
+                  ),
+                ),
+              ),
             ),
-          )
+          );
         },
       ),
-    
     );
   }
 }
